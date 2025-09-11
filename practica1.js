910c73e6b2df27563dd58d5b9baf5f6688ef0961
Rardiel <salasceballorardiel@gmail.com>
2025-09-11 12:44:21 -0400

    Añadido de .ts y .js

---
+++ practica1.js
@@ -0,0 +1,8 @@
+var framework = "Angular";
+var anioLanzamiento = 2016;
+// Luego haga un console.log
+console.log("Nombre de la variable 'framework':", framework);
+console.log("Nombre de la variable 'anioLanzamiento':", anioLanzamiento);
+// Demostración: 'let' puede cambiar de valor
+framework = "React";
+console.log("La variable 'framework' ha cambiado a:", framework);
---
+++ practica1.ts
@@ -0,0 +1,10 @@
+let framework: string = "Angular";
+const anioLanzamiento: number = 2016;
+
+
+console.log("Nombre de la variable 'framework':", framework);
+console.log("Nombre de la variable 'anioLanzamiento':", anioLanzamiento);
+
+
+framework = "React";
+console.log("La variable 'framework' ha cambiado a:", framework);
